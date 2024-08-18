6e6d18d70075dd0875a8c17e0cfb51af44697aa4
Asad Mirza <asadbmirza97@gmail.com>
2024-08-18 15:33:15 -0400

    Added in logic for the game as well as card styling, pokemon randomize each step and if you get a match wrong the pokemon change

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,75 @@ import Card from "./components/card";
 
 function App() {
   const [pokemon, setPokemon] = useState(new Array(15));
+  const [shuffle, setShuffle] = useState(false);
+  const [reset, setReset] = useState(false);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+  if (score > highScore) setHighScore(score);
 
   useEffect(() => {
     let addPokemon = new Array(15);
+    let keys = new Array(15);
+
     for (let i = 0; i < 15; i++) {
       let newPokemon = Math.round(Math.random() * 800);
-      while(addPokemon.includes(newPokemon)) {
+      while (addPokemon.includes(newPokemon)) {
         newPokemon = Math.round(Math.random() * 800);
       }
-      console.log(newPokemon);
       addPokemon[i] = newPokemon;
+      keys[i] = Math.random();
     }
-    setPokemon(addPokemon);
-  }, []);
+    setPokemon(
+      addPokemon.map((name, index) => (
+        <Card
+          key={keys[index]}
+          name={name}
+          setShuffle={setShuffle}
+          setReset={setReset}
+          isClicked={false}
+        />
+      ))
+    );
+    setScore(0);
+    if (reset == true) alert("Try Again!");
+    setReset(false);
+  }, [reset]);
+
+
+  
+
+  if (shuffle == true) {
+    let array = pokemon.slice();
+
+    let currentIndex = array.length;
+
+    while (currentIndex != 0) {
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    if (reset != true) {
+      setScore(score + 1);
+    }
+  
+    setShuffle(false);
+    setPokemon(array);
+  }
+
+
 
   return (
     <>
-      <p>Pokemon image</p>
-      {pokemon.map((name) => (
-        <Card name={name} />
-      ))}
+      <h1>Pokemon Memory Game!</h1>
+      <p>Score: {score}</p>
+      <p>High Score: {highScore}</p>
+      {score < 15 ? <div className="cards">{pokemon}</div> : <h1>You win!</h1>}
     </>
   );
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import "./card.css";
 
-function Card({ name }) {
+function Card({ name , setShuffle, setReset, isClicked}) {
   const [pokemonImg, setPokemonImg] = useState("");
   const [pokemonName, setPokemonName] = useState("");
   const [loaded, setLoaded] = useState(false);
+  const [clicked, setClicked] = useState(isClicked);
+
   useEffect(() => {
     async function pokemon(monster) {
       try {
@@ -22,12 +24,27 @@ function Card({ name }) {
     pokemon(name);
   }, []);
 
-  console.log(pokemonImg);
+  
+
+  function handleClick(e)  {
+    if (clicked == true) {
+        setClicked(false);
+        setReset(true);
+    }
+    else {
+        setClicked(true);
+        
+    }
+    setShuffle(true);
+    
+  }
+
+  
 
   return (
     <>
       {loaded && (
-        <div className="card">
+        <div className="card" onClick={handleClick}>
           <h2>{pokemonName}</h2>
           <img src={pokemonImg} alt="" />
         </div>
